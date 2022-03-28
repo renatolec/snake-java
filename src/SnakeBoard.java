3ad408b3580f15a113dad610fcdeae7e4bee1d5d
Renato Leite Camilo <rl.camilo@unesp.br>
2022-03-28 13:11:27 -0300

    Correção de erros de matriz

--- src/SnakeBoard.java
+++ src/SnakeBoard.java
@@ -8,25 +8,19 @@ public class SnakeBoard extends JPanel {
     final int SIZE = DIMENSION / BLOCK;
     final int NONE = 0, SNAKE = 1, APPLE = 2;
 
-    int x[] = new int[SIZE];
-    int y[] = new int[SIZE];
-    int[][] map = new int[SIZE][SIZE];
-
-    int bodyParts = 1;
+    int x[] = new int[SIZE * SIZE];
+    int y[] = new int[SIZE * SIZE];
     int xApple, yApple;
+    int bodyParts = 1;
 
     boolean appleOnBoard = false;
 
     SnakeBoard() {
         setPreferredSize(new Dimension(DIMENSION, DIMENSION));
         x[0] = y[0] = 0;
-        map[x[0]][y[0]] = SNAKE;
     }
 
     public void movementSnake(int direction) {
-
-        int last = bodyParts - 1;
-        map[x[last]][y[last]] = NONE;
 
         for (int i = bodyParts; i > 0; i--) {
             x[i] = x[i - 1];
@@ -48,9 +42,8 @@ public class SnakeBoard extends JPanel {
                 break;
         }
 
-        if(map[x[0]][y[0]] == APPLE)
+        if (x[0] == xApple && y[0] == yApple)
             eatApple();
-        map[x[0]][y[0]] = SNAKE;
     }
 
     public void genApple() {
@@ -59,7 +52,6 @@ public class SnakeBoard extends JPanel {
         Random gen = new Random();
         xApple = gen.nextInt(SIZE);
         yApple = gen.nextInt(SIZE);
-        map[xApple][yApple] = APPLE;
         appleOnBoard = true;
     }
 
@@ -70,21 +62,28 @@ public class SnakeBoard extends JPanel {
 
     @Override
     public void paintComponent(Graphics g) {
+        //PLANO DE FUNDO
         for (int i = 0; i < SIZE; i++) {
             for (int j = 0; j < SIZE; j++) {
-                if (map[i][j] == APPLE) {
-                    g.setColor(new Color(255, 0, 0));
-                    g.fillRect(i * BLOCK, j * BLOCK, BLOCK, BLOCK);
-                }
-                if (map[i][j] == NONE) {
-                    g.setColor(new Color(255, 255, 255));
-                    g.fillRect(i * BLOCK, j * BLOCK, BLOCK, BLOCK);
-                }     
-            }          
+                g.setColor(new Color(255, 255, 255));
+                g.fillRect(i * BLOCK, j * BLOCK, BLOCK, BLOCK);
+            }
         }
-        for(int i = 0 ; i < bodyParts; i++){
+
+        //OBJETOS
+        for (int i = 0; i < bodyParts; i++) {
             g.setColor(new Color(0, 255, 0));
             g.fillRect(x[i] * BLOCK, y[i] * BLOCK, BLOCK, BLOCK);
+        }
+        g.setColor(new Color(255, 0, 0));
+        g.fillOval(xApple * BLOCK, yApple * BLOCK, BLOCK, BLOCK);
+
+        //MALHA QUADRICULADA
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                g.setColor(new Color(0, 0, 0));
+                g.drawRect(i * BLOCK, j * BLOCK, BLOCK, BLOCK);
+            }
         }
     }
 }
