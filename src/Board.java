818db33bb4a03c888faa27c684c7de9b403d62eb
Renato Leite Camilo <rl.camilo@unesp.br>
2022-03-28 02:34:16 -0300

    Adicionada movimentação primitiva

--- src/Board.java
+++ src/Board.java
@@ -1,20 +1,47 @@
 import javax.swing.*;
 import java.awt.*;
+import java.util.Map;
 
 public class Board extends JPanel {
 
     final int DIMENSION = 320, BLOCK = 16;
     final int SIZE = DIMENSION / BLOCK;
+    final int NONE = 0, PLAYER = 1;
+
+    int x0, y0;
+    int[][] map = new int[SIZE][SIZE];
 
     Board() {
         setPreferredSize(new Dimension(DIMENSION, DIMENSION));
+        x0 = y0 = 0;
+        map[x0][y0] = PLAYER;
+    }
+
+    public void move(int movex, int movey){
+        int x1 = x0 + movex;
+        int y1 = y0 +movey;
+        if(x1 >= 0 && y1 >= 0 && x1 < SIZE && y1 < SIZE){
+            map[x1][y1] = PLAYER;
+            map[x0][y0] = 0;
+            x0 = x1;
+            y0 = y1;
+        }
     }
 
     @Override
     public void paintComponent(Graphics g) {
-        g.setColor(new Color(0, 0, 0));
         for (int i = 0; i < SIZE; i++) {
             for (int j = 0; j < SIZE; j++) {
+                if(map[i][j] == PLAYER){
+                    g.setColor(new Color(0, 255, 0));
+                    g.fillRect(i*BLOCK , j*BLOCK, BLOCK, BLOCK);
+                    g.setColor(new Color(0, 0, 0));
+                }
+                if(map[i][j] == NONE){
+                    g.setColor(new Color(255, 255, 255));
+                    g.fillRect(i*BLOCK , j*BLOCK, BLOCK, BLOCK);
+                    g.setColor(new Color(0, 0, 0));
+                }
                 g.drawRect(i*BLOCK , j*BLOCK, BLOCK, BLOCK);
             }
         }
--- src/Snake.java
+++ src/Snake.java
@@ -1,8 +1,11 @@
 import javax.swing.JFrame;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyAdapter;
 
 public class Snake extends JFrame {
 
     Board board;
+    int command;
 
     Snake(){
         super("Snake - Arcade Game");
@@ -14,6 +17,29 @@ public class Snake extends JFrame {
 
         add(board);
         pack();
+
+        addKeyListener(new KeyAdapter(){
+            public void keyPressed(KeyEvent e){
+                if (e.getKeyCode() == KeyEvent.VK_UP) {
+                    board.move(0, -1);
+                } else if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+                    board.move(1, 0);
+                } else if (e.getKeyCode() == KeyEvent.VK_DOWN) {
+                    board.move(0, 1);
+                } else if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+                   board.move(-1, 0);
+                }
+            }
+        });
+
+        new Thread(new Runnable(){
+            public void run(){
+                while(true){
+                    
+                    board.repaint();
+                }
+            }
+        }).start();
     }
 
     public static void main(String[] args) throws Exception {
